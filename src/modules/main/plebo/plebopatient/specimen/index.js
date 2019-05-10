d09f7645b70fa8744b76b5785a16c7ebb148eb4d
Karen Sigua <ksigua@myherohub.com>
2019-05-10 18:28:47 +0800

    Fixed functions on plebo specimen table

--- src/modules/main/plebo/plebopatient/specimen/index.js
+++ src/modules/main/plebo/plebopatient/specimen/index.js
@@ -1,92 +1,96 @@
 import React from 'react';
-import { Row, Col, Table, Button, } from 'antd';
+import { Row, Col, Table, Typography, Button } from 'antd';
 
 import './specimen.css';
 
+const { Text } = Typography;
+
 class SpecimenList extends React.Component {
-  state = {
-    Status: 'PENDING'
+
+ // React lifecycle
+  componentDidMount() 
+  {
+    this.setState({
+      showLoading: false
+    });
   }
 
   onPress = () => {
     this.setState({
-      Status: ''
+      StatusPending: 'PENDING'
     })
   }
 
- // React lifecycle
- componentDidMount() 
- {
-   this.setState({
-     showLoading: false
-   });
- }
- 
   expandedRowRender = () => {
     const columns = [
       {
         dataIndex: 'Specimen',
         key: 'Specimen',
       },
+      { 
+        dataIndex: 'StatusPending', 
+        key: 'StatusPending',
+        render: text =>  <Text onClick={this.onSampleIDClick} className="sampleLabTestID">{text}</Text>,
+      },
     ];
 
-    const data = [];
-    const testspecimen = ['CBC', 'Hemoglobin', 'Hematocrit'];
+  const data = [];
+  const testspecimen = ['CBC', 'Hemoglobin', 'Hematocrit'];
     for (let i = 1; i < 4; i+=1) {
       data.push({
         Specimen: testspecimen[Math.floor(Math.random() * testspecimen.length)],
       });
     }
 
-  return (
-	<div className="child-no-header-table">
-      <Table
+    return (
+    <div className="child-no-header-table">
+      <Table 
         columns={columns}
         dataSource={data}
         pagination={false}
         size="small"
       />
-      </div>
+    </div>
     );
   };
 
   render() {  
     const columns = [
-    { 
-      title: 'SPECIMEN', 
-      dataIndex: 'Specimen', 
-      key: 'Specimen',
-    },
-    { 
-      title: 'STATUS', 
-      dataIndex: 'StatusPending', 
-      key: 'StatusPending',
-      render: button =>  <Button onClick={this.onSampleIDClick} className="sampleLabTestID">{button}</Button>,
-    },
+      { 
+        title: 'SPECIMEN', 
+        dataIndex: 'Specimen', 
+        key: 'Specimen',
+      },
+      { 
+        title: 'STATUS', 
+        dataIndex: 'StatusPending', 
+        key: 'StatusPending',
+      },
     ];
     const data = [];
     const testspecimen = ['Blood', 'Serum'];
-    for (let i = 1; i < 15; ++i) {
+
+      for (let i = 1; i < 15; ++i) {
         data.push({
         Specimen: testspecimen[Math.floor(Math.random() * testspecimen.length)],
-        StatusPending: 'PENDING',
-      });
-    }
-    return (
-      <div>
-        <Row>
-          <Col lg={24} xs={24}>
-                <Table
-                  columns={columns}
-                  expandedRowRender={this.expandedRowRender}
-                  dataSource={data}
-                  size="small"
-                  scroll={{ y: 300 }}
-                />
-          </Col>
-        </Row>
-      </div>
-    );
+        StatusPending: "PENDING",
+        });
+      }
+      return (
+        <div>
+          <Row>
+            <Col lg={24} xs={24}>
+              <Table
+                columns={columns}
+                expandedRowRender={this.expandedRowRender}
+                dataSource={data}
+                size="small"
+                scroll={{ y: 300 }}
+              />
+            </Col>
+          </Row>
+        </div>
+      );
   }
 }
 export default SpecimenList;
