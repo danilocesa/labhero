c8bb2f38238c76672303bb40fbccd6edc71e435d
Jer <jpesquera@myherohub.com>
2019-09-06 10:51:26 +0800

    Settings - Exam Request. Added full function of update and add

---
+++ src/modules/main/settings/exam_item_jer/update_form/constant.js
@@ -0,0 +1,16 @@
+const FIELD_RULES = {
+	examItemName: [
+    { required: true, message: 'This field is required.' }
+	],
+	examItemGeneralName: [
+    { required: true, message: 'This field is required.' }
+	],
+	resultType: [
+    { required: true, message: 'This field is required.' }
+	],
+	integrationCode: [
+    { required: true, message: 'This field is required.' }
+	],
+};
+
+export default FIELD_RULES;
