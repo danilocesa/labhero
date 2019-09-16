c847241776c94a1216f611af59555724ddc28ab9
Dan Cesa <dcesa@myherohub.com>
2019-09-16 18:32:29 +0800

    Added inventory settings for storage & sections

--- src/modules/main/inventory/settings/index.js
+++ src/modules/main/inventory/settings/index.js
@@ -5,17 +5,27 @@ import { Tabs as AntTabs} from 'antd';
 // CUSTOM MODULES
 import InventoryPageCrumb from 'modules/main/inventory/settings/shared_components/page_bread_crumb';
 import InventorySupplier from 'modules/main/inventory/settings/supplier';
+import InventoryStorage from 'modules/main/inventory/settings/storage';
+import InventorySection from 'modules/main/inventory/settings/sections';
 
 //  CONSTANTS
 const { TabPane } = AntTabs;
+const pageTitleSettings = "INVENTORY / SETTINGS /";
+const pageTitle = [
+  `${pageTitleSettings } SUPPLIERS`,
+  `${pageTitleSettings } SUPPLIERS`, 
+  `${pageTitleSettings } STORAGE`,
+  `${pageTitleSettings } SECTION`,
+  `${pageTitleSettings } CATEGORIES`
+]
 
 class InventorySettings extends React.Component {
 	state = { 
-    pageTitle: "Inventory/Settings/Suppliers"
+    pageTitle: pageTitle[0]
   }
 
-  handleTabChange = (event) => {
-    console.log("TCL: InventorySettings -> handleTabChange -> event", event)
+  handleTabChange = (key) => {
+    this.setState({ pageTitle: pageTitle[key] })
   }
 
   render() {
@@ -27,10 +37,10 @@ class InventorySettings extends React.Component {
             <InventorySupplier />
           </TabPane>
           <TabPane tab="STORAGE/LOCATION" key="2">
-            Content of Tab Pane 2
+            <InventoryStorage />
           </TabPane>
           <TabPane tab="SECTION" key="3">
-            Content of Tab Pane 3
+            <InventorySection />
           </TabPane>
           <TabPane tab="CATEGORIES" key="4">
             Content of Tab Pane 4
