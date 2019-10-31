0ee951034062c0bdb1681016a72e053a102ae7f1
Dan Cesa <dcesa@myherohub.com>
2019-10-31 11:38:49 +0800

    Search Result: Fix expanded table result

--- src/modules/main/search_lab_result/index.jsx
+++ src/modules/main/search_lab_result/index.jsx
@@ -1,37 +1,21 @@
+// LIBRARY
 import React from 'react';
-import { Row, Col } from 'antd';
-import axiosCall from 'services/axiosCall';
+import { Row as AntRow, Col as AntCol } from 'antd';
 
+// CUSTOM MODULES
 import WrappedSearchLabTestForm from './search_form';
 import WrapperSearchLabTestResultList from './search_result';
 
 class SearchLabTestResult extends React.Component {
-	UNSAFE_componentWillMount() {
-		this.testApi()
-	}
-
-	testApi = async () =>  {
-		const userData = JSON.parse(sessionStorage.getItem('LOGGEDIN_USER_DATA'));
-		const response = await axiosCall({
-			method: 'GET',
-			url: 'Patient/name/test',
-			headers: {
-				authorization: `Bearer ${userData.token}`
-			}
-		});
-
-		console.log(response);
-		return response;
-	}
 	
 	render() {
     return (
-	    <Row type="flex" align="middle" justify="center">
-		    <Col xs={24}>
+	    <AntRow type="flex" align="middle" justify="center">
+		    <AntCol xs={24}>
 			    <WrappedSearchLabTestForm />
 			    <WrapperSearchLabTestResultList />
-		    </Col>
-	    </Row>
+		    </AntCol>
+	    </AntRow>
     );
   }
 }
--- src/modules/main/search_lab_result/search_result/index.jsx
+++ src/modules/main/search_lab_result/search_result/index.jsx
@@ -1,14 +1,13 @@
 // LIBRARY
 import React from 'react';
-import { Row, Col, Table, Select, Drawer, Typography, Empty, Card } from 'antd';
+import { Row as AntRow, Col as AntCol, Table, Select, Drawer as AntDrawer, Typography, Empty as AntEmpty, Card as AntCard} from 'antd';
 
 // CUSTOM MODULES
+import axiosCall from 'services/axiosCall';
 import PatientInfo from '../../patientinfo';
-
 
 // CSS
 import './searchresult.css';
-import axiosCall from 'services/axiosCall';
 
 // CONSTANTS
 const { Option } = Select;
@@ -22,12 +21,6 @@ class WrapperSearchLabTestResultList extends React.Component {
 	};
 
   // React lifecycle
-  UNSAFE_componentWillMount() {
-    // axiosCall({ method: 'get', url: 'waiverChecker', params: { waiver_id: 1, token: cookies.get('user_token') } }).then((resp) => {
-    //   const showMods = resp.data.data.length !== 0 ? this.setState({ showModal: false }) : null
-    //   return showMods
-    // })
-  }  
  
   componentDidMount() 
   {
@@ -35,10 +28,6 @@ class WrapperSearchLabTestResultList extends React.Component {
       showLoading: false
     });
   } 
-
- 
-
-
 
   // Custom function
   onSampleIDClick = () => {
@@ -53,47 +42,19 @@ class WrapperSearchLabTestResultList extends React.Component {
     });
   }
 
-  expandedRowRender = async (record) => {
-    // const columns = [
-    //   {
-    //     title: 'REQUEST DATE',
-    //     dataIndex: 'RequestDate',
-    //     key: 'RequestDate',
-    //     align: 'center',
-    //     width:'25%',
-    //   },
-    //   {
-    //     title: 'SAMPLE ID',
-    //     dataIndex: 'SampleId',
-    //     key: 'SampleId',
-    //     render: text =>  <Text onClick={this.onSampleIDClick} className="sampleLabTestID">{text}</Text>,
-    //     width:'25%',
-    //   },
-    //   { title: 'STATUS', 
-    //     dataIndex: 'Status', 
-    //     key: 'Status',
-    //     width:'25%',
-    //   },
-    //   { title: 'HISLINK', 
-    //     dataIndex: 'HisLink', 
-    //     key: 'HisLink',
-    //     width:'25%',
-    //   },
-    // ];
+  expandedRowRender = () => {
     const data = [];
-    const teststatus = ['On-going', 'Verified', 'Cancelled'];
     for (let i = 1; i < 20; i+=1) {
       data.push({
         key: i*1000,
         RequestDate: '04/11/2019',
         SampleId: `100${i}`,
-        Status: teststatus[Math.floor(Math.random() * teststatus.length)],
-        HisLink: `190411200i${i}`,
+        Status: 'test',
+       // HisLink: `190411200i${i}`,
       });
 		}
-		
-		const exams = await this.fetchExams();
-		const columns = [
+		console.log(data);
+		const expandedtablecolumns = [
       {
         title: 'sectionID',
         dataIndex: 'sectionID',
@@ -105,7 +66,6 @@ class WrapperSearchLabTestResultList extends React.Component {
         title: 'sectionName',
         dataIndex: 'sectionName',
         key: 'SampleId',
-        render: text =>  <Text onClick={this.onSampleIDClick} className="sampleLabTestID">{text}</Text>,
         width:'25%',
       },
       { title: 'sectionCode', 
@@ -114,13 +74,11 @@ class WrapperSearchLabTestResultList extends React.Component {
         width:'25%',
       },
     ];
-		
-		console.log(exams);
-
+    
     return (
 	    <Table
-		    columns={columns}
-		    dataSource={exams}
+		    columns={expandedtablecolumns}
+		    dataSource={data}
 		    pagination={false}
 		    size="small"
 		    scroll={{ y: 200 }}
@@ -128,54 +86,38 @@ class WrapperSearchLabTestResultList extends React.Component {
 		);
   };
 
-	fetchExams = async () => {
-		const response = await axiosCall({
-			method: 'GET',
-			url: 'Section',
-		});
-
-		return response.data;
-	}
-
   render() {  
     const columns = [
     { 
       title: 'PATIENT ID', 
       dataIndex: 'PatientID', 
-      key: 'PatientID',
-      sorter: (a, b) => a.PatientID - b.PatientID, 
+      key: 'PatientID' 
     },
     { 
       title: 'HOSPITAL ID', 
       dataIndex: 'HospitalID', 
-      key: 'HospitalID',
-      sorter: (a, b) => a.HospitalID - b.HospitalID, 
+      key: 'HospitalID'
     },
     { title: 'LAST NAME', 
       dataIndex: 'LastName', 
       key: 'LastName',
-      defaultSortOrder: 'ascend',
-      sorter: (a, b) => { return a.LastName.localeCompare(b.LastName)}, 
+      defaultSortOrder: 'ascend'
       },
     { title: 'FIRST NAME', 
       dataIndex: 'FirstName', 
-      key: 'FirstName',
-      sorter: (a, b) => { return a.FirstName.localeCompare(b.FirstName)},
+      key: 'FirstName'
       },
     { title: 'DATE OF BIRTH', 
       dataIndex: 'DateOfBirth', 
-      key: 'DateOfBirth',
-      sorter: (a, b) => a.DateOfBirth.length - b.DateOfBirth.length,
+      key: 'DateOfBirth'
     },
     { title: 'GENDER', 
       dataIndex: 'Gender', 
-      key: 'Gender',
-      sorter: (a, b) => { return a.Gender.localeCompare(b.Gender)},
+      key: 'Gender'
     },
     { title: 'ADDRESS', 
       dataIndex: 'Address', 
-      key: 'Address',
-      sorter: (a, b) => { return a.Address.localeCompare(b.Address)},
+      key: 'Address'
     },
     ];
     const data = [];
@@ -197,31 +139,31 @@ class WrapperSearchLabTestResultList extends React.Component {
         Address: testcityaddress[Math.floor(Math.random() * testcityaddress.length)],
       });
     }
-    const emptyTableData = <Card><Empty /></Card>
+    const emptyTableData = <AntCard><AntEmpty /></AntCard>
     return (
 	    <div>
-		    <Row style={{ paddingBottom: '0.5em' }} type="flex">
-			    <Col md={12} className="gutter-row">
+		    <AntRow style={{ paddingBottom: '0.5em' }} type="flex">
+			    <AntCol md={12} className="gutter-row">
 				    <Text strong> Search Lab Result </Text>
 				    <br />
 				    <Text> Showing <b>0</b> items out of <b>0</b> results </Text>
-			    </Col>
-			    <Col md={10} className="gutter-row" style={{ lineHeight : '2.5em'}}>
+			    </AntCol>
+			    <AntCol md={10} className="gutter-row" style={{ lineHeight : '2.5em'}}>
 				    <Text className="gutter-box" style={{ float:'right', marginRight:'1em' }}>
               Display per page 
 				    </Text>
-			    </Col>
-			    <Col className="gutter-row">
+			    </AntCol>
+			    <AntCol className="gutter-row">
 				    <Select defaultValue="10" style={{ width: '100%' }} className="gutter-box">
 					    <Option value="10">10</Option>
 					    <Option value="20">20</Option>
 					    <Option value="50">50</Option>
 					    <Option value="100">100</Option>
 				    </Select>
-			    </Col>
-		    </Row>
-		    <Row type="flex">
-			    <Col lg={24} xs={24}>
+			    </AntCol>
+		    </AntRow>
+		    <AntRow type="flex">
+			    <AntCol lg={24} xs={24}>
 						<Table
 							className="searchLabTestResultTable"
 							columns={columns}
@@ -230,18 +172,18 @@ class WrapperSearchLabTestResultList extends React.Component {
 							size="small"
 							scroll={{ y: 300 }}
 							loading={isLoading}
-							onExpand={(exapanded, record) => { console.log('onexpand'); }}
+							onExpand={() => { console.log('onexpand'); }}
 						/>
-			    </Col>
-		    </Row>
-					<Drawer
+			    </AntCol>
+		    </AntRow>
+					<AntDrawer
 						title="Patient information"
 						onClose={this.onClosePatientInfoDrawer}
 						width="80%"
 						visible={this.state.showPatientInfo}
 					>
 					<PatientInfo /> 
-					</Drawer>
+					</AntDrawer>
 	    </div>
     );
   }
